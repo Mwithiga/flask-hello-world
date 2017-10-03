646d14a2d448e6eba12871f4c26c1f6d1ee6f272
eins <Mwithigajames@outlook.com>
2017-10-03 09:18:30 +0300

    added dynamic routes

--- app.py
+++ app.py
@@ -10,10 +10,28 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/route")
 
-# dfine the view using a function, which retursn a string
+# define the view using a function, which retursn a string
 
 def hello_world():
 	return "Hell, World!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print(value + 1)
+	return "correct"
+@app.route("/float/<float:value>")
+def float_type(value):
+	print(value + 1)
+	return "correct"
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+	print(value)
+	return "correct"
 
 #start the dvelopment server using the run() method
 if __name__ =="__main__":
